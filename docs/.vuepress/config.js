a5502274bc6f1fb1632dbee3da869a39864e0dfa
Artist <2550115859@qq.com>
2020-01-05 20:39:34 +0800

    发布成功

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -11,7 +11,7 @@ module.exports = {
           lang: 'zh-CN', // 将会被设置为 <html> 的 lang 属性
         }
     },
-    base: '/vuepressBlog',
+    base: '/vuepressBlog/',//如果你打算发布到 https://<USERNAME>.github.io/，则可以省略这一步，因为 base 默认即是 "/"。如果你打算发布到 https://<USERNAME>.github.io/<REPO>/（也就是说你的仓库在 https://github.com/<USERNAME>/<REPO>），则将 base 设置为 "/<REPO>/"。
     themeConfig: {//导航链接
     //   navbar: false,//禁用所有导航栏
       repo: 'WkArtist/vuepressBlog',
