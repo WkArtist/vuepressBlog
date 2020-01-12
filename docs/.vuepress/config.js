a8d51f17dea49892595070793a9d346d55e65152
Artist <2550115859@qq.com>
2020-01-12 13:41:38 +0800

    hgfhffsdf

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -87,7 +87,8 @@ module.exports = {
       // 默认是 false, 设置为 true 来启用
       editLinks: true,
       // 默认为 "Edit this page"
-      editLinkText: '帮助我们改善此页面！'
+      editLinkText: '帮助我们改善此页面！',
+      docsDir: 'docs/'
     }
   }
   
